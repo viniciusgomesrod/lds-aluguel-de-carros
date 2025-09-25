a0dfda2854e50358a0e28df173a60d22a3321480
viniciusgomesrod <viniciusgomes2202@gmail.com>
2025-09-25 15:35:37 -0300

    classe credito sem regras de negocio

--- Codigo/Backend/src/main/java/Codigo/Backend/models/Credito.java
+++ Codigo/Backend/src/main/java/Codigo/Backend/models/Credito.java
@@ -1,5 +1,29 @@
 package Codigo.Backend.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "creditos")
 public class Credito {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private double valor;
+    private Integer parcelas;
+    private double valorParcelas;
+    
+    @Enumerated(EnumType.STRING)
+    private StatusCredito statusCredito;
+    
     
 }
---
+++ Codigo/Backend/src/main/java/Codigo/Backend/models/StatusCredito.java
@@ -0,0 +1,7 @@
+package Codigo.Backend.models;
+
+public enum StatusCredito {
+    ANALISE,
+    APROVADO,
+    NEGADO
+}
